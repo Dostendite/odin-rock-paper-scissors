297aab2e8ffa0e02349b27527ccd64c693e1e5d1
dostendite <dostendite@outlook.es>
2023-09-05 23:34:25 +0200

    Improve getComputerChoice function
    Removed the getRandomNumber function which
    getComputerChoice depended on and added a random
    number variable to the getComputerChoice function,
    also changed the if structure to a switch one.

--- scripts.js
+++ scripts.js
@@ -1,21 +1,18 @@
 // Rock Paper Scissors
 
-function getRandomNumber() {
-    // get random number (1, 2, 3)
-
-    return Math.floor(Math.random() * 3) + 1;
-}
-
 function getComputerChoice() {
-    let randomNumber = getRandomNumber();
 
-    if (randomNumber === 1) {
-        return "Rock";
-    } else if (randomNumber === 2) {
-        return "Paper";
-    } else {
-        return "Scissors";
+    let randomNumber = ~~(Math.random() * 3);
+
+    switch (randomNumber) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
     }
+
 }
 
 function getPlayerChoice () {
