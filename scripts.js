c9d41631464c987286f45caef30abd53760a27b6
dostendite <dostendite@outlook.es>
2023-09-05 22:53:53 +0200

    Finish base game
    The game is now playable, first it logs the round number,
    then it gets the computer & player choices, and then
    compares them to declare a winner, after 5 rounds
    the game is over.
    
    The base game has been finished, I mostly used comments /
    guidelines from the curriculum to guide myself in making
    the program, though there is a lot to be improved.
    
    If I were to remake the program from scratch, I would
    do it using a notebook and drawing visual diagrams to better
    understand the problem at hand and then plan accordingly,
    as well as add error handling etc, this project will be
    revisited later on the curriculum, so I might do that then.
    
    Note: I should have done more commits along the way.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,115 @@
 console.log("Hello World!");
 
-// Rock, paper, scissors game
+// Rock Paper Scissors
 
-// getComputerChoice function
+function getRandomNumber() {
+    // get random number (1, 2, 3)
 
-// write a function that plays a single round
-// of Rock Paper Scissors, it should take two
-// parameters: playerSelection & computerSelection
+    return Math.floor(Math.random() * 3) + 1;
+}
 
-// then returrn a string that declares the winner
-// of the round like: "You lose! Paper beats Rock"
+function getComputerChoice() {
+    let randomNumber = getRandomNumber();
 
-// make the playerSelection parameter case-insensitive
-// -> rock, ROCK, RocK are all valid
+    if (randomNumber === 1) {
+        return "Rock";
+    } else if (randomNumber === 2) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+function getPlayerChoice () {
+    const playerSelection = prompt("Choose your weapon (Rock, Paper, Scissors): ");
+
+    // can add error handling later
+    if (playerSelection[0].toLowerCase() === "r") {
+        return "Rock";
+    } else if (playerSelection[0].toLowerCase() === "p") {
+        return "Paper";
+    } else  if (playerSelection[0].toLowerCase() === "s") {
+        return "Scissors";
+    }
+}
+
+
+function playRound(playerSelection, computerSelection) {
+    // play rps round
+
+    // string to print at the end
+    let result;
+
+    // if true, the player wins, if false, the computer wins
+    let playerWins;
+
+    // if playerSelection is equal to computerSelection,
+    // the result is a tie, else compare:
+
+    // playerSelection || computerSelection || winner
+    // Rock            || Paper             || computer
+    // Rock            || Scissors          || player
+
+    // Paper           || Rock              || player
+    // Paper           || Scissors          || computer
+
+    // Scissors        || Rock              || computer
+    // Scissors        || Paper             || player
+
+    // compare
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Paper") {
+            playerWins = false;
+        } else if (computerSelection === "Scissors") {
+            playerWins = true;
+        }
+    }
+
+    if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            playerWins = true;
+        } else if (computerSelection === "Scissors") {
+            playerWins = false;
+        }
+    }
+
+    if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+            playerWins = false;
+        } else if (computerSelection === "Paper") {
+            playerWins = true;
+        }
+    }
+
+    // check for a tie first, if not, check for a winner
+    
+    if (playerSelection === computerSelection) {
+    result = `It's a tie! ${playerSelection} ties with ${computerSelection}!`
+    } else if (playerWins) {
+        result = `Player wins! ${playerSelection} beats ${computerSelection}!`
+    } else {
+        result = `Computer wins! ${computerSelection} beats ${playerSelection}!`
+    }
+
+    // return a string that declares the winner
+    // of the round like: "You lose! Paper beats Rock"
+    return result;
+}
+
+
+// ~~~~~~~~ PROGRAM START ~~~~~~~~
+
+let roundsPlayed = 1;
+
+while (roundsPlayed < 6) {
+
+    // Play five rounds
+
+    console.log(`Round number ${roundsPlayed}`);
+    const computerSelection = getComputerChoice();
+    const playerSelection = getPlayerChoice();
+
+    // Log result and add a round to the counter, then restart
+    console.log(playRound(playerSelection, computerSelection));
+    roundsPlayed++;
+}
